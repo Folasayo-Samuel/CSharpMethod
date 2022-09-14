d4dfdb362c67e261e9efa05cbb2c129b04269a0e
Folasayo Samuel <folasayosamuel@gmail.com>
2022-09-14 16:24:42 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace CSharpMethod
+{
+	class Program
+	{
+		static void Main(string[]args)
+		{
+			SayHi("Folasayo", 25);
+			SayHi("Samuel", 24);
+			SayHi("Olayemi", 23);
+
+            var cubeNumber = cube(5);
+            Console.WriteLine(cubeNumber);
+
+
+			Console.ReadLine();
+		}
+		
+		public static void SayHi(string name, int age)
+		{
+			Console.WriteLine($"Hello {name}");
+		}
+		
+		public static int cube(int num)
+		{
+			var result  = num * num * num;
+			return result;
+		}
+		
+	}
+}
